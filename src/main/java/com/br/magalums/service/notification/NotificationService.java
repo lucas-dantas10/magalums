6c37fb9f2413f4f199abcac45f01111e87e33beb
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-07-26 18:31:38 -0300

    feat: add service to search a notification

--- src/main/java/com/br/magalums/service/notification/NotificationService.java
+++ src/main/java/com/br/magalums/service/notification/NotificationService.java
@@ -1,8 +1,11 @@
 package com.br.magalums.service.notification;
 
 import com.br.magalums.dto.notification.ScheduleNotificationDto;
+import com.br.magalums.entity.Notification;
 import com.br.magalums.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class NotificationService implements NotificationServiceInterface {
@@ -16,5 +19,10 @@ public class NotificationService implements NotificationServiceInterface {
     @Override
     public void scheduleNotification(ScheduleNotificationDto dto) {
         this.notificationRepository.save(dto.toNotification());
+    }
+
+    @Override
+    public Optional<Notification> findbyId(Long notificationId) {
+        return this.notificationRepository.findById(notificationId);
     }
 }
--- src/main/java/com/br/magalums/service/notification/NotificationServiceInterface.java
+++ src/main/java/com/br/magalums/service/notification/NotificationServiceInterface.java
@@ -1,7 +1,11 @@
 package com.br.magalums.service.notification;
 
 import com.br.magalums.dto.notification.ScheduleNotificationDto;
+import com.br.magalums.entity.Notification;
+
+import java.util.Optional;
 
 public interface NotificationServiceInterface {
     void scheduleNotification(ScheduleNotificationDto dto);
+    Optional<Notification> findbyId(Long notificationId);
 }
